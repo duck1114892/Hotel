9e6ec79a38dd9213346d0a4276692751a776078e
duck1114892 <lenguyenhongduca6cl21@gmail.com>
2023-11-23 14:12:51 +0700

    fix

--- src/pages/viewOrder.jsx
+++ src/pages/viewOrder.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { callHistory } from "../service/api"
-import { Button, Col, List, Row, Typography } from "antd"
+import { Button, Col, Layout, List, Row, Table, Typography } from "antd"
 import { Link } from "react-router-dom"
 import { ArrowLeftOutlined } from "@ant-design/icons"
 import AvataComponent from "../component/avataComponent"
 import '../../public/scss/viewOrder.css'
+import { Content, Footer, Header } from "antd/es/layout/layout"
 const ViewOrder = () => {
     const [data, setData] = useState()
     const [loading, setLoading] = useState(false)
@@ -13,7 +14,6 @@ const ViewOrder = () => {
         const getData = async () => {
             try {
                 const res = await callHistory()
-
                 setData(res.data)
                 setLoading(true)
 
@@ -27,57 +27,63 @@ const ViewOrder = () => {
         }
         getData()
     }, [])
-
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+        {
+            title: 'phone',
+            dataIndex: 'phone',
+        },
+        {
+            title: 'Detail',
+            dataIndex: 'detail',
+            render: (detail) => (
+                <>
+                    {detail.map((item) => (
+                        <div className="listItem" key={item._id}>
+                            {item.bookName} x {item.quantity}
+                        </div>
+                    ))}
+                </>
+            ),
+        },
+        {
+            title: 'Total',
+            dataIndex: 'totalPrice',
+            sorter: {
+                compare: (a, b) => a.totalPrice - b.totalPrice,
+                multiple: 1,
+            },
+        },
+    ];
+    const onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
     return (
         <>
-            <div className="container" style={{ display: "block", height: "80vh", backgroundColor: "white" }}>
-                <div className="headers" style={{ width: "100%", display: "flex", justifyContent: "space-around", marginTop: "2%", height: "1vh", backgroundColor: "white" }} >
-                    <Link to="/"><Button className="backBtn" style={{
-                        position: "absolute",
-                        left: '100px',
-                        fontSize: '20px',
-                        height: '45px'
-                    }}><ArrowLeftOutlined ></ArrowLeftOutlined></Button></Link>
-                    <div className="text" style={{ fontSize: "40px" }}>History</div>
-                    <AvataComponent props={true}></AvataComponent>
-                </div>
-                <div className="body" style={{ width: "100%", height: "60vh", display: "flex", justifyContent: "center" }}>
-                    <List
-                        style={{ width: "100%", height: "70vh", }}
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <List.Item
-                                style={{ marginLeft: "10%" }}
-                            >
-                                <List.Item.Meta
-                                    title={(<>
-                                        <Row>
-                                            <Col span={7}>
-                                                <a style={{ color: "black" }}>{item.name} </a>
-                                            </Col>
-                                            <Col span={7}>
-                                                <a >{item.email}</a>
-                                            </Col>
-                                            <Col span={7}>
-                                                <a className="font-size" style={{ color: "red" }}>{item.totalPrice.toLocaleString('vn-VN', { style: 'currency', currency: 'VND' })}</a>
-                                            </Col>
-                                        </Row>
+            <Layout style={{ height: "100vh" }}>
+                <Header style={{ height: "7vh", width: "100%", backgroundColor: "white" }}>
+                    <Row style={{ display: 'flex', justifyContent: "center", width: "100%", textAlign: "center" }}>
+                        <Col span={8}> <Link to="/"><Button className="backBtn" style={{
 
-                                    </>)}
-                                    description={
-                                        (<>{item.detail.map((item) => {
-                                            return (<ul>
-                                                <li>{item.bookName} x {item.quantity}</li>
-                                            </ul>)
-                                        })}</>)
-                                    }
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-            </div>
+                        }}>
+                            <ArrowLeftOutlined ></ArrowLeftOutlined></Button>
+                        </Link></Col>
+                        <Col span={8}>  <div className="text" style={{ fontSize: "40px", }}>History</div></Col>
+                        <Col span={8}> <AvataComponent props={true}></AvataComponent></Col>
+                    </Row>
+                </Header>
+                <Content style={{}}>
+                    <Table columns={columns} dataSource={data} onChange={onChange} />
+                </Content>
+
+            </Layout>
         </>
     )
 }
